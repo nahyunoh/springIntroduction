db7b7edae70669e365de3618684e684a37bacbd8
nahyunoh <onheh28@gmail.com>
2023-02-12 23:43:16 +0900

    인프런 스프링입문

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //http의 바디 부에 이 데이터를 직접 넣어 주겠다
+    public String helloString(@RequestParam("name")String name){
+        return "hello "+name;    //"hello spring"
+        //template 과의 차이 : view 없이 요청 클라이언트에 그냥 데려감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello{
+        private String name;
+
+        //프로퍼티 접근 방식
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
