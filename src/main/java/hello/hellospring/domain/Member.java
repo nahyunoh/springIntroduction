46749bbe3f98b6b39171d65d8fd2c925b7c6b435
nahyunoh <onheh28@gmail.com>
2023-03-08 23:25:24 +0900

    22.03.08

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,19 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "username")
     private String name;
 
     public Long getId() {
